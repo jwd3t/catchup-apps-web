135723a23531a36585c4cd768cf61b4dabf3486b
juan <ju@a>
2026-04-24 17:45:19 -0500

    feaat(i18n):  integrate vue-i18n with english and spanish translations

--- src/main.js
+++ src/main.js
@@ -7,9 +7,11 @@ import Material from '@primeuix/themes/material';
 import './style.css';
 import {Avatar, Button, Card, Drawer, Image, Menu,
     Menubar, SelectButton, Toolbar, Tooltip} from "primevue";
+import i18n from "./i18n.js";
 
 
 createApp(App)
+    .use(i18n)
     .use(PrimeVue, {ripple: true, theme: {preset: Material}})
     .component('pv-button', Button)
     .component('pv-select-button', SelectButton)
